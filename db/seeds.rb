75da03ad1692a2007fcfbe942c25650e096de356
CHRIS <NWAKAUC1@GMAIL.COM>
2023-10-05 20:07:03 +0200

    Add png icons

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,22 +3,21 @@ Company.destroy_all
 
 # Seed data for Company with icons
 [
-  Rails.root.join('public', 'icons', 'visa-black.svg'),  
-  Rails.root.join('db', 'icons', 'tyme-bank-black.svg'),
-  Rails.root.join('db', 'icons', 'distell-black.svg'),
-  Rails.root.join('db', 'icons', 'spotify-black.svg'),
-  Rails.root.join('db', 'icons', 'microsoft-black.svg'),
-  Rails.root.join('db', 'icons', 'engen-black.svg'),
-  Rails.root.join('db', 'icons', 'nike-black.svg'),
-  Rails.root.join('db', 'icons', 'wesgrow-black.svg'),
-  Rails.root.join('db', 'icons', 'multichoice-black.svg'),
-  Rails.root.join('db', 'icons', 'pnp-black.svg'),
-  Rails.root.join('db', 'icons', 'liquid-black.svg'),
-  Rails.root.join('db', 'icons', 'tfg-black.svg'),
-  Rails.root.join('db', 'icons', 'sanlam-black.svg'),
-  Rails.root.join('db', 'icons', 'santam-black.svg'),
-  Rails.root.join('db', 'icons', 'bbc-black.svg'),
-
+  Rails.root.join('public', 'icons', 'visa-black.svg'),
+  Rails.root.join('public', 'icons', 'tyme-bank-black.svg'),
+  Rails.root.join('public', 'icons', 'distell-black.svg'),
+  Rails.root.join('public', 'icons', 'spotify-black.svg'),
+  Rails.root.join('public', 'icons', 'microsoft-black.svg'),
+  Rails.root.join('public', 'icons', 'engen-black.svg'),
+  Rails.root.join('public', 'icons', 'nike-black.svg'),
+  Rails.root.join('public', 'icons', 'wesgrow-black.svg'),
+  Rails.root.join('public', 'icons', 'multichoice-black.svg'),
+  Rails.root.join('public', 'icons', 'pnp-black.svg'),
+  Rails.root.join('public', 'icons', 'liquid-black.svg'),
+  Rails.root.join('public', 'icons', 'tfg-black.svg'),
+  Rails.root.join('public', 'icons', 'sanlam-black.svg'),
+  Rails.root.join('public', 'icons', 'santam-black.svg'),
+  Rails.root.join('public', 'icons', 'bbc-black.svg'),
 ].each do |icon_path|
   company = Company.new
   company.icon.attach(io: File.open(icon_path), filename: File.basename(icon_path))
